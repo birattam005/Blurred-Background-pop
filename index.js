3201b155e414b9bdff398fa5b7215c2b052c828c
Birattam Rajak <birattam444@gmail.com>
2023-03-22 21:56:07 +0530

    adding js

---
+++ index.js
@@ -0,0 +1,17 @@
+const containerEl = document.querySelector(".container");
+const btnEl = document.querySelector(".btn");
+const popContainerEl = document.querySelector(".pop-container");
+const closeIconEl = document.querySelector(".close-icon");
+
+
+btnEl.addEventListener("click", () =>{
+    // containerEl.classList.add("active");
+    popContainerEl.classList.remove("active");
+});
+
+closeIconEl.addEventListener("click", () =>{
+    // containerEl.classList.remove("active");
+    popContainerEl.classList.add("active");
+});
+
+
